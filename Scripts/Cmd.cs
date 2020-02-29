fbdaded70727a071bb7096616ec34acffbb31e46
ShujiOba <shuji365630@gmail.com>
2020-02-29 22:01:25 +0900

    :sparkles: Update Cmd

--- Scripts/Cmd.cs
+++ Scripts/Cmd.cs
@@ -2,56 +2,28 @@ using System.Threading.Tasks;
 
 namespace UniTEA
 {
-  public struct Cmd<T> where T : struct
-  {
-
-    public static Cmd<T> none = new Cmd<T>(null);
-
-    Task<IMessenger<T>> task
+    public struct Cmd<T> where T : struct
     {
-      get;
-      set;
+        public static Cmd<T> none = new Cmd<T>(_ => { });
+
+        public static Cmd<T> batch(Cmd<T>[] cmds)
+        {
+            return new Cmd<T>(d =>
+            {
+                foreach (Cmd<T> cmd in cmds)
+                {
+                    cmd.exec(d);
+                }
+            });
+        }
+
+        System.Action<System.Action<IMessenger<T>>> task;
+
+        public void exec(System.Action<IMessenger<T>> d) => this.task(d);
+
+        public Cmd(System.Action<System.Action<IMessenger<T>>> task)
+        {
+            this.task = task;
+        }
     }
-
-    public Cmd(Task<IMessenger<T>> task)
-    {
-      this.task = task;
-    }
-
-    public async Task<IMessenger<T>> Preform()
-    {
-      IMessenger<T> msg = await task;
-      return msg;
-    }
-
-    public override bool Equals(object obj)
-    {
-      if (obj == null || !(obj is Cmd<T>))
-      {
-        return false;
-      }
-      return (Cmd<T>)obj == this;
-    }
-
-    public override int GetHashCode()
-    {
-      return task.GetHashCode();
-    }
-
-    public static bool operator ==(Cmd<T> msg1, Cmd<T> msg2)
-    {
-      if (msg1.task == null || msg2.task == null)
-      {
-        return msg1.task == msg2.task;
-      }
-
-      return msg1.task.Id == msg2.task.Id;
-    }
-
-    public static bool operator !=(Cmd<T> msg1, Cmd<T> msg2)
-    {
-      return !(msg1 == msg2);
-    }
-
-  }
 }
